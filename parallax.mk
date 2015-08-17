a73939cdf38ed337a2184dbfad846ad63cb5e9d4
joshfox87 <xlxfoxxlx@gmail.com>
2015-08-17 17:46:40 -0400

    Update and rename slim.mk to parallax.mk

--- parallax.mk
+++ parallax.mk
@@ -1,5 +1,5 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/parallax/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := shamu
@@ -9,17 +9,17 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+$(call inherit-product, vendor/parallax/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/parallax/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/shamu/aosp_shamu.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := shamu
-PRODUCT_NAME := slim_shamu
+PRODUCT_NAME := parallax_shamu
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6
 PRODUCT_MANUFACTURER := motorola
